438e5c3c3c9e2545d31db13ab5dadd162596b588
Mell <141835304+mellkarine@users.noreply.github.com>
2024-11-05 21:51:44 -0300

    Create AppDatabase.kt

---
+++ src/main/exampleMaravilhasLocalizacao/AppDatabase.kt
@@ -0,0 +1,30 @@
+package com.example.maravilhaslocalizacao
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Historico::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun historicoDao(): HistoricoDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "app_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
+
+
